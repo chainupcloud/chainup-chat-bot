5d4919af45f74d1ff6672b52ec94d1846cc37441
RenShiWei <ren_shi_wei@qq.com>
2024-07-10 19:41:33 +0800

    chat langchain

--- chatbot_langchain.py
+++ chatbot_langchain.py
@@ -9,7 +9,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import FastEmbedEmbeddings
 # from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores.utils import filter_complex_metadata
+from langchain_community.vectorstores.utils import filter_complex_metadata
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
@@ -25,8 +25,8 @@ modelSel = ""
 
 
 # Load the PDF file to ChromaDB
-def loadDataFromPDFFile(filePath):
-    loader = PyPDFLoader(filePath)
+def load_data_from_pdf(file_path):
+    loader = PyPDFLoader(file_path)
     pages = loader.load_and_split()
     chunks = filter_complex_metadata(pages)
     vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
@@ -34,25 +34,25 @@ def loadDataFromPDFFile(filePath):
     return vector_store
 
 
-def modelResponse(message, history):
+def model_response(message, history):
     # llm = ChatOllama(model = conf["model"])
     llm = Ollama(model="llama3")
 
     prompt = PromptTemplate(template=promptTemplate, input_variables=["context", "question"])
 
     # Initiate the retriever
-    dbLoaded = loadDataFromPDFFile("training_pdfs/ChainUp Introduction.pdf")
-    retriever = dbLoaded.as_retriever(search_type="similarity_score_threshold", search_kwargs={
+    db_loaded = load_data_from_pdf("training_pdfs/ChainUp Introduction.pdf")
+    retriever = db_loaded.as_retriever(search_type="similarity_score_threshold", search_kwargs={
         "k": 5,
         "score_threshold": 0.2
     })
-    hpChain = (
+    hp_chain = (
             {"context": retriever, "question": RunnablePassthrough()}
             | prompt
             | llm
             | StrOutputParser()
     )
-    return hpChain.invoke(message)
+    return hp_chain.invoke(message)
 
 
 if __name__ == "__main__":
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     #     conf = json.load(confFile)
     #     print(conf["model"])
 
-    chatUI = gr.ChatInterface(fn=modelResponse,
+    chatUI = gr.ChatInterface(fn=model_response,
                               title="Your Friendly Web3 Chatbot",
                               description="Ask me about anything about Web3 and ChainUp."
                               )
